5c8e9ce2a2b991fbe2972a9d3bf59b207c273145
Ierga <i.ergakoff@gmail.com>
2024-01-31 21:25:37 +0300

    GB - Spring. Homework 2. Start

---
+++ src/main/java/ru/ergakov/gb/HW2Application.java
@@ -0,0 +1,13 @@
+package ru.ergakov.gb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HW2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HW2Application.class, args);
+	}
+
+}
---
+++ src/main/java/ru/ergakov/gb/service/UserService.java
@@ -0,0 +1,27 @@
+package ru.ergakov.gb.service;
+
+import ru.ergakov.gb.model.User;
+import ru.ergakov.gb.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    //public void deleteById(int id)
+}
